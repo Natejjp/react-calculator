ce1fbf47c6b394ce10b2c9115760f04f060340fb
Nathan Phame <diyf2021@gmail.com>
2021-06-03 13:17:26 -0400

    Basic html and css layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World!</div>
+  return (
+    <main>
+      <section>
+        <ul>
+          <li className="input">0</li>
+          <li className="clear">AC</li>
+          <li>/</li>
+          <li>7</li>
+          <li>8</li>
+          <li>9</li>
+          <li>*</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <li>+</li>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>-</li>
+          <li className="zero">0</li>
+          <li>.</li>
+          <li>=</li>
+        </ul>
+        {/* <ul></ul> */}
+      </section>
+    </main>
+  )
 }
