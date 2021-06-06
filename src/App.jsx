ee9a51487fd254ef0ecae44932408333d7c66d57
Nathan Phame <diyf2021@gmail.com>
2021-06-06 18:23:00 -0400

    Able to add numbers in console!

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,47 @@
 import React, { useState } from 'react'
 
 export function App() {
-  const [answer, setAnswer] = useState(0)
+  const [displayNumber, setDisplayNumber] = useState(0)
+  const [firstNumber, setFirstNumber] = useState(0)
+  const [secondNumber, setSecondNumber] = useState(0)
+
+  function add() {
+    const test = displayNumber
+
+    setFirstNumber(test)
+
+    console.log(test)
+  }
+
+  function equal() {
+    const pineapple = displayNumber + firstNumber
+    console.log(pineapple)
+  }
 
   return (
     <main>
       <section>
         <ul>
-          <li className="input">{answer}</li>
+          <li className="input">{displayNumber}</li>
           <li className="clear">AC</li>
           <li>/</li>
-          <li onClick={() => setAnswer(7)}>7</li>
-          <li onClick={() => setAnswer(8)}>8</li>
-          <li onClick={() => setAnswer(9)}>9</li>
+          <li onClick={() => setDisplayNumber(7)}>7</li>
+          <li onClick={() => setDisplayNumber(8)}>8</li>
+          <li onClick={() => setDisplayNumber(9)}>9</li>
           <li>*</li>
-          <li onClick={() => setAnswer(4)}>4</li>
-          <li onClick={() => setAnswer(5)}>5</li>
-          <li onClick={() => setAnswer(6)}>6</li>
-          <li>+</li>
-          <li onClick={() => setAnswer(1)}>1</li>
-          <li onClick={() => setAnswer(2)}>2</li>
-          <li onClick={() => setAnswer(3)}>3</li>
+          <li onClick={() => setDisplayNumber(4)}>4</li>
+          <li onClick={() => setDisplayNumber(5)}>5</li>
+          <li onClick={() => setDisplayNumber(6)}>6</li>
+          <li onClick={add}>+</li>
+          <li onClick={() => setDisplayNumber(1)}>1</li>
+          <li onClick={() => setDisplayNumber(2)}>2</li>
+          <li onClick={() => setDisplayNumber(3)}>3</li>
           <li>-</li>
-          <li className="zero" onClick={() => setAnswer(0)}>
+          <li className="zero" onClick={() => setDisplayNumber(0)}>
             0
           </li>
           <li>.</li>
-          <li>=</li>
+          <li onClick={equal}>=</li>
         </ul>
       </section>
     </main>
